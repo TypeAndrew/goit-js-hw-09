87bf38b9b74a98e2958ee17422be074dbf8afdd1
TypeAndrew <andy.shular@gmail.com>
2022-11-01 16:38:18 +0200

    task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,81 +1,83 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-let dtPicker = document.querySelector('input[type="text"]');
-let btnStart = document.querySelector('button[data-start=""]');
-let dataDays = document.querySelector('span[data-days=""]');
-let dataHours = document.querySelector('span[data-hours=""]');
-let dataMinutes = document.querySelector('span[data-minutes=""]');
-let dataSeconds = document.querySelector('span[data-seconds=""]');
-let deltaTime, leftTime;
 
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        console.log(selectedDates[0]);
+class TimeCounter {
 
-        if (selectedDates[0] < new Date()) {
-            console.log("Please choose a date in the future");
-            btnStart.setAttribute('disabled', 'disabled');
 
-        } else {
-            btnStart.removeAttribute('disabled');
-            deltaTime = selectedDates[0] - new Date();
-        }
-    },
-};
+    constructor() {
+        this.btnStart = document.querySelector('button[data-start=""]');
+        this.dtPicker = document.querySelector('input[type="text"]');
+        this.dataDays = document.querySelector('span[data-days=""]');
+        this.dataHours = document.querySelector('span[data-hours=""]');
+        this.dataMinutes = document.querySelector('span[data-minutes=""]');
+        this.dataSeconds = document.querySelector('span[data-seconds=""]');
+        this.deltaTime, this.leftTime;
 
-flatpickr(dtPicker, options);
 
-function addLeadingZero(leftTime) {
-
-    for (const key in leftTime) {
-        if (Object.hasOwnProperty.call(leftTime, key)) {
-            leftTime[key].toString().padStart(2, "0");
-
-        }
     }
 
+    convertMs(ms) {
+        // Number of milliseconds per unit of time
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+        // Remaining days
+        const days = Math.floor(ms / day);
+        // Remaining hours
+        const hours = Math.floor((ms % day) / hour);
+        // Remaining minutes
+        const minutes = Math.floor(((ms % day) % hour) / minute);
+        // Remaining seconds
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+        return { days, hours, minutes, seconds };
+    }
+
+    addLeadingZero(times) {
+        return times.toString().padStart(2, "0");
+    }
+
+    Init() {
+
+
+        let deltaTime;
+        let options = {
+            enableTime: true,
+            time_24hr: true,
+            defaultDate: new Date(),
+            minuteIncrement: 1,
+            onClose(selectedDates) {
+                console.log(selectedDates[0]);
+
+                if (selectedDates[0] < new Date()) {
+                    console.log("Please choose a date in the future");
+                    btnStart.setAttribute('disabled', 'disabled');
+
+                } else {
+                    btnStart.removeAttribute('disabled');
+                    deltaTime = selectedDates[0] - new Date();
+                }
+            },
+        };
+
+        flatpickr(this.dtPicker, options);
+
+        this.btnStart.addEventListener('click', () => {
+            setInterval(() => {
+                deltaTime = deltaTime - 1000;
+                let { days, hours, minutes, seconds } = this.convertMs(deltaTime);
+
+                this.dataDays.textContent = this.addLeadingZero(days);
+                this.dataHours.textContent = this.addLeadingZero(hours);
+                this.dataMinutes.textContent = this.addLeadingZero(minutes);
+                this.dataSeconds.textContent = this.addLeadingZero(seconds);
+            }, 1000);
+        });
+    }
 }
 
-function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-    return { days, hours, minutes, seconds };
-}
-
-function addLeadingZero(times) {
-    return times.toString().padStart(2, "0");
-}
-
-btnStart.addEventListener('click', () => {
-    setInterval(() => {
-        deltaTime = deltaTime - 1000;
-        let { days, hours, minutes, seconds } = convertMs(deltaTime);
-
-
-        dataDays.textContent = addLeadingZero(days);
-        dataHours.textContent = addLeadingZero(hours);
-        dataMinutes.textContent = addLeadingZero(minutes);
-        dataSeconds.textContent = addLeadingZero(seconds);
-
-
-        // addLeadingZero(leftTime);
-    }, 1000);
-});
+let timeCounter = new TimeCounter();
+timeCounter.Init();
