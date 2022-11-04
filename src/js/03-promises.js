401db70b12424314be8f183c817dcbf8a960c8d3
shulyar <andy.shular@gmail.com>
2022-11-04 10:48:31 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+
+
+let timerId;
+const buttonCP = document.querySelector('form');
+
+buttonCP.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const dalay = document.querySelector('input[name="delay"]').value;
+  const step = document.querySelector('input[name="step"]').value;
+  const amount = document.querySelector('input[name="amount"]').value;
+  for (let i = 0; i < amount; i++){
+  const timerId = setTimeout(createPromise(i, dalay)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }).catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    })
+    , step);
+  }  
+     
+});
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  
+  
+  const promise = new Promise(function (resolve, reject) {
+  const shouldResolve = Math.random() > 0.5;
+    
+    let result = {"position": position, "delay":  delay};
+    if (shouldResolve) {
+      // Fulfill
+      resolve(result);
+    } else {
+      // Reject
+      reject(result);s
+    }
+  });
+  return promise;
 }
+
+
+
+
